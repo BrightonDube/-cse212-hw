6094b0a5b2f8ab0f87bd210cb9774a525d80f606
BrightonDube <BrightonDube@users.noreply.github.com>
2025-09-11 05:04:35 +0200

    playing around

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -8,6 +8,21 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
+        var numbers = new int[3];
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        int[] ages = { 20, 21, 30, 56 };
+        // or: var ages = new int[] { 20, 21, 30 };
+        string[] meals = { "breakfast", "dinner", "lunch" };
+        var songs = new string[3] { "hallelujah", "Roisin", "Diana" };
+        var cats = new List<string> { "Bob", "Luno", "Milo" };
+
+        // join the whole array, not a single element
+        Console.WriteLine(string.Join(", ", songs));
+        Console.WriteLine(string.Join(", ", ages));
+        Console.WriteLine(numbers[1]);
+        Console.WriteLine(string.Join(", ", cats));
     }
 }
